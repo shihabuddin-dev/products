c5c619776a20bb3038c893701503fe921d586907
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-03-29 03:03:16 +0600

    i have finally added here dynamically data by api and made it dynamic data

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,14 +1,35 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
+
 import Product from '../product/Product';
 
 const Products = ({ productsPromise }) => {
     const products = use(productsPromise)
+
+    const [getProduct, setGetProduct] = useState([])
+
+    const handleGetProduct = (product) => {
+        const newProduct = [...getProduct, product]
+        setGetProduct(newProduct)
+    }
     return (
         <div>
             <h1 className='text-3xl text-center pb-4 font-semibold'>Total Products: {products.length}</h1>
+            <h2 className='text-xl font-semibold pb-4'>Added To Cart: {getProduct.length}</h2>
+            <div className='grid md:grid-cols-3 gap-4 pb-4'>
+                {
+                    getProduct.map(getPro => <div key={getPro.id} className='flex gap-2 items-center'>
+                        <img className='w-12' src={getPro.image} alt="this is product images" />
+                        <p>{getPro.title} <br /> ${getPro.price}</p>
+
+                    </div>)
+                }
+            </div>
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                 {
-                    products.map(product => <Product key={product.id} product={product}></Product>)
+                    products.map(product => <Product
+                        key={product.id}
+                        handleGetProduct={handleGetProduct}
+                        product={product}></Product>)
                 }
             </div>
         </div>
